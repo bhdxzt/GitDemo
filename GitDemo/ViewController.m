df8c64eb8b4664e28bdab20c7256fd7e95b2a299
bhdxzt <sap360@kanoutekiMac-mini.local>
2017-11-13 17:35:27 +0800

    add change
    add change

--- GitDemo/ViewController.m
+++ GitDemo/ViewController.m
@@ -17,6 +17,12 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    /*
+     1
+     2
+     3
+     4
+     */
 }
 
 
