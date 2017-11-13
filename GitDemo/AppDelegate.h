bf447b830f856000c64f433c25cfe0d0f34b5953
bhdxzt <sap360@kanoutekiMac-mini.local>
2017-11-13 17:26:16 +0800

    test
    test

---
+++ GitDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitDemo
+//
+//  Created by SAP360 on 2017/11/13.
+//  Copyright © 2017年 SAP. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
